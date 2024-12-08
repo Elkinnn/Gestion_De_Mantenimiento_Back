11ab071c17fc46db09cfd19641860023aad8f8db
Elkin <elkinjoao2004@hotmail.com>
2024-12-08 17:42:25 -0500

    Se modifico el codigo para que al momento de iniciar sesión el usuario puede visualizar tanto su nombre como su rol

--- controllers/activoController.js
+++ controllers/activoController.js
@@ -4,19 +4,20 @@ const activoController = {
   // Obtener todos los activos con los nombres de las relaciones
   getAllActivos: (req, res) => {
     const query = `
-      SELECT 
-        a.proceso_compra,
-        a.codigo,
-        a.nombre,
-        a.estado,
-        u.nombre AS ubicacion,
-        t.nombre AS tipo,
-        p.nombre AS proveedor
-      FROM activos a
-      LEFT JOIN ubicaciones u ON a.ubicacion_id = u.id
-      LEFT JOIN tipos_activos t ON a.tipo_activo_id = t.id
-      LEFT JOIN proveedores p ON a.proveedor_id = p.id
-    `;
+  SELECT 
+    a.id,  -- Asegúrate de seleccionar el campo id
+    a.proceso_compra,
+    a.codigo,
+    a.nombre,
+    a.estado,
+    u.nombre AS ubicacion,
+    t.nombre AS tipo,
+    p.nombre AS proveedor
+  FROM activos a
+  LEFT JOIN ubicaciones u ON a.ubicacion_id = u.id
+  LEFT JOIN tipos_activos t ON a.tipo_activo_id = t.id
+  LEFT JOIN proveedores p ON a.proveedor_id = p.id
+`;
 
     db.query(query, (err, results) => {
       if (err) {
